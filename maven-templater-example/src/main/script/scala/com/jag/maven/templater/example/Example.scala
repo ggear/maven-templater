36c307eeeeaaf9339830dbf530d14064d8d8fb6a
ggear <graham.gear@gmail.com>
2017-07-16 12:37:59 +0800

    Update doc

--- maven-templater-example/src/main/script/scala/com/jag/maven/templater/example/Example.scala
+++ maven-templater-example/src/main/script/scala/com/jag/maven/templater/example/Example.scala
@@ -1,25 +1,23 @@
 /*
-    PRE-PROCESSED SCRIPT, DO NOT EDIT
+# **PRE-PROCESSED SCRIPT - DO NOT EDIT**
 
-    This file is in the SCRIPT pre-processed state with template available by the
-    same file name and package under the following directory:
+This file is in the *SCRIPT* pre-processed state with template available by the
+same package and file name under the modules src/main/template directory.
 
-    /Users/graham/_/dev/graham/maven-templater/maven-templater-example/src/main/template
-
-    When editing the template directly (as indicated by the presence of the
-    ${TEMPLATE.PRE-PROCESSOR.*} tag at the top of this file), care should be
-    taken to ensure the maven-resources-plugin generate-sources filtering of
-    the ${TEMPLATE.PRE-PROCESSOR.*} tags, which comment and or uncomment blocks
-    of the template, leave the file in a consistent state post filtering. It is
-    desirable that in template form the file remains runnable and or compilable
-    in at least one form (ie script or library) for ease of editing.
+When editing the template directly (as indicated by the presence of the
+TEMPLATE.PRE-PROCESSOR.RAW_TEMPLATE tag at the top of this file), care should
+be taken to ensure the maven-resources-plugin generate-sources filtering of the
+TEMPLATE.PRE-PROCESSOR tags, which comment and or uncomment blocks of the
+template, leave the file in a consistent state post filtering. It is desirable
+that in template form, the file remains runnable and or compilable as a script
+or library for ease of editing.
 */
 
 /*
 %AddJar https://repo.maven.apache.org/maven2/org/apache/commons/commons-csv/1.4/commons-csv-1.4.jar
 */
 
-/* TEMPLATE OPEN COMMENT SCRIPT
+/***IGNORE LIBRARY BOILERPLATE - START**
 
 package com.jag.maven.templater.example
 
@@ -29,7 +27,7 @@ object Example {
 
   def example(tokens: util.List[String]): String = {
 
-*/
+**IGNORE LIBRARY BOILERPLATE - FINISH***/
 
 import java.io.StringReader
 
@@ -38,7 +36,7 @@ import org.apache.commons.csv.CSVFormat
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
 
-// TEMPLATE OPEN UNCOMMENT SCRIPT
+//
 
 // Provide example parameters
 var tokens = new java.util.ArrayList[String]()
@@ -46,7 +44,13 @@ tokens.add("1")
 tokens.add("2")
 tokens.add("3")
 
-// TEMPLATE CLOSE UNCOMMENT SCRIPT
+//
+
+/*
+A silly example that takes a Java List of Strings, forms a single CSV of them,
+then parses each String out again using CSVFormat and then finally builds
+and returns a new CSV String from the tokens - I told you it was silly!
+ */
 
 val tokensProcessed = ListBuffer[String]()
 CSVFormat.RFC4180.parse(new StringReader(tokens.asScala.mkString(","))).asScala.foreach { record =>
@@ -54,9 +58,9 @@ CSVFormat.RFC4180.parse(new StringReader(tokens.asScala.mkString(","))).asScala.
 }
 tokensProcessed.mkString(",")
 
-/* TEMPLATE OPEN COMMENT SCRIPT
+/***IGNORE LIBRARY BOILERPLATE - START**
 
   }
 }
 
-*/
+**IGNORE LIBRARY BOILERPLATE - FINISH***/
--- maven-templater-example/src/main/template/scala/com/jag/maven/templater/example/Example.scala
+++ maven-templater-example/src/main/template/scala/com/jag/maven/templater/example/Example.scala
@@ -1,18 +1,16 @@
 /*
-    ${TEMPLATE.PRE-PROCESSOR.RAW_TEMPLATE}
+${TEMPLATE.PRE-PROCESSOR.RAW_TEMPLATE}
 
-    This file is in the ${TEMPLATE.PRE-PROCESSOR.STATE} pre-processed state with template available by the
-    same file name and package under the following directory:
+This file is in the ${TEMPLATE.PRE-PROCESSOR.STATE} pre-processed state with template available by the
+same package and file name under the modules src/main/template directory.
 
-    ${project.basedir}/src/main/template
-
-    When editing the template directly (as indicated by the presence of the
-    ${TEMPLATE.PRE-PROCESSOR.*} tag at the top of this file), care should be
-    taken to ensure the maven-resources-plugin generate-sources filtering of
-    the ${TEMPLATE.PRE-PROCESSOR.*} tags, which comment and or uncomment blocks
-    of the template, leave the file in a consistent state post filtering. It is
-    desirable that in template form the file remains runnable and or compilable
-    in at least one form (ie script or library) for ease of editing.
+When editing the template directly (as indicated by the presence of the
+TEMPLATE.PRE-PROCESSOR.RAW_TEMPLATE tag at the top of this file), care should
+be taken to ensure the maven-resources-plugin generate-sources filtering of the
+TEMPLATE.PRE-PROCESSOR tags, which comment and or uncomment blocks of the
+template, leave the file in a consistent state post filtering. It is desirable
+that in template form, the file remains runnable and or compilable as a script
+or library for ease of editing.
 */
 
 /*
@@ -47,6 +45,12 @@ tokens.add("2")
 tokens.add("3")
 
 //${TEMPLATE.PRE-PROCESSOR.CLOSE}
+
+/*
+A silly example that takes a Java List of Strings, forms a single CSV of them,
+then parses each String out again using CSVFormat and then finally builds
+and returns a new CSV String from the tokens - I told you it was silly!
+ */
 
 val tokensProcessed = ListBuffer[String]()
 CSVFormat.RFC4180.parse(new StringReader(tokens.asScala.mkString(","))).asScala.foreach { record =>
