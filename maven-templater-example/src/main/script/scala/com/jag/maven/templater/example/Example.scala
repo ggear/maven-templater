7797e51b2a802f857d0909117da300e2cf238396
ggear <graham.gear@gmail.com>
2017-07-23 15:04:56 +0800

    Update README

--- maven-templater-example/src/main/script/scala/com/jag/maven/templater/example/Example.scala
+++ maven-templater-example/src/main/script/scala/com/jag/maven/templater/example/Example.scala
@@ -1,8 +1,8 @@
 /*
-${TEMPLATE.PRE-PROCESSOR.SPACER}
-${TEMPLATE.PRE-PROCESSOR.RAW_TEMPLATE}
+***
+# **PRE-PROCESSED SCRIPT - EDITS WILL BE CLOBBERED BY MAVEN BUILD**
 
-This file is in the ${TEMPLATE.PRE-PROCESSOR.STATE} pre-processed state with template available by the
+This file is in the *SCRIPT* pre-processed state with template available by the
 same package and file name under the modules src/main/template directory.
 
 When editing the template directly (as indicated by the presence of the
@@ -23,7 +23,7 @@ The library can be tested during the standard maven compile and test phases.
 Note that pre-processed files will be overwritten as part of the Maven build
 process. Care should be taken to either ignore and not edit these files (eg
 libraries) or check them in and note changes post Maven build (eg scripts)
-${TEMPLATE.PRE-PROCESSOR.SPACER}
+***
 */
 
 
@@ -32,11 +32,11 @@ ${TEMPLATE.PRE-PROCESSOR.SPACER}
 // to a SNAPSHOT, timestamped version. If you are looking for an example, best
 // to checkout a release, https://github.com/ggear/maven-templater/releases
 /*
-%AddJar http://52.63.86.162/artifactory/cloudera-framework-releases/com/jag/maven-templater/maven-templater-assembly/${templater.version}/maven-templater-assembly-${templater.version}.jar
+%AddJar http://52.63.86.162/artifactory/cloudera-framework-releases/com/jag/maven-templater/maven-templater-assembly/1.2.3-SNAPSHOT/maven-templater-assembly-1.2.3-SNAPSHOT.jar
 */
 
 /*
-${TEMPLATE.PRE-PROCESSOR.UNCLOSE}
+**IGNORE LIBRARY BOILERPLATE - START**
 
 package com.jag.maven.templater.example
 
@@ -44,20 +44,24 @@ object Example {
 
   def example(tokens: java.util.List[String]): String = {
 
-${TEMPLATE.PRE-PROCESSOR.UNOPEN}
+**IGNORE LIBRARY BOILERPLATE - FINISH**
 */
 
 import com.jag.maven.templater.TemplaterUtil
 
-//${TEMPLATE.PRE-PROCESSOR.OPEN}
+//
 // Add dependencies dynamically
-//${TEMPLATE.PRE-PROCESSOR.ADDJAROPEN}
+//
+//@formatter:off
+kernel.magics.addJar(
 TemplaterUtil.getDepJar(
-  "com.jag.maven-templater", "maven-templater-example", "${project.version}", "jar-with-dependencies",
+  "com.jag.maven-templater", "maven-templater-example", "1.2.3-SNAPSHOT", "jar-with-dependencies",
   "http://52.63.86.162/artifactory/cloudera-framework-releases",
   "http://52.63.86.162/artifactory/cloudera-framework-snapshots")
-//${TEMPLATE.PRE-PROCESSOR.ADDJARCLOSE}
-//${TEMPLATE.PRE-PROCESSOR.CLOSE}
+//
+    + " -f")
+//@formatter:on
+//
 
 import java.io.StringReader
 
@@ -67,20 +71,20 @@ import org.apache.commons.csv.CSVFormat
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ListBuffer
 
-//${TEMPLATE.PRE-PROCESSOR.OPEN}
+//
 // Provide example parameters
 val tokens = new java.util.ArrayList[String]()
 tokens.add("1")
 tokens.add("2")
 tokens.add("3")
-//${TEMPLATE.PRE-PROCESSOR.CLOSE}
+//
 
 /*
-${TEMPLATE.PRE-PROCESSOR.SPACER}
+***
 A silly example that takes a Java List of Strings, forms a single CSV of them,
 then parses each String out again using CSVFormat and then finally builds
 and returns a new CSV String from the tokens - I told you it was silly!
-${TEMPLATE.PRE-PROCESSOR.SPACER}
+***
 */
 
 val tokensProcessed = ListBuffer[String]()
@@ -90,16 +94,16 @@ CSVFormat.RFC4180.parse(new StringReader(tokens.asScala.mkString(ExampleUtil.Del
 val tokensString = tokensProcessed.mkString(ExampleUtil.Delimiter)
 tokensString
 
-//${TEMPLATE.PRE-PROCESSOR.OPEN}
+//
 // Test the result
 if (tokens.asScala.mkString(",") != tokensString) throw new AssertionError("Expected [" + tokens.asScala.mkString(",") + "] but got [" + tokensString + "]")
-//${TEMPLATE.PRE-PROCESSOR.CLOSE}
+//
 
 /*
-${TEMPLATE.PRE-PROCESSOR.UNCLOSE}
+**IGNORE LIBRARY BOILERPLATE - START**
 
   }
 }
 
-${TEMPLATE.PRE-PROCESSOR.UNOPEN}
+**IGNORE LIBRARY BOILERPLATE - FINISH**
 */
